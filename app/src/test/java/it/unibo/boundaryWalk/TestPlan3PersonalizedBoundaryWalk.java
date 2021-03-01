640a3fa62dbf73c4ae907130274524e48f52e22c
LucaLand <luca.landolfi3@studio.unibo.it>
2021-03-01 20:19:44 +0100

    Finished Implementation of working TestPlan3-Personalized!

--- app/src/test/java/it/unibo/boundaryWalk/TestPlan3PersonalizedBoundaryWalk.java
+++ app/src/test/java/it/unibo/boundaryWalk/TestPlan3PersonalizedBoundaryWalk.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
-public class TestPlan3BoundaryWalk {
+public class TestPlan3PersonalizedBoundaryWalk {
     private MoveVirtualRobot appl;
 
     @Before
@@ -23,32 +23,63 @@ public class TestPlan3BoundaryWalk {
 
     @Test
     public void testPlan3BoundaryWalk(){
-        int perimetro = 10;
-        double RobotLen = 1.5;
+        //Dati del committente in metri
+        double lR = 2.4, lC = 2.4;
+        double perimetro = (lR*2)+(lC*2);
+        double RobotLen = 0.6;
         double RobotSpeed = 0.2;
         int time = (int) (100 * RobotLen/RobotSpeed);
+        int homePosR = 1, homePosC = 1; //Cella di partenza
 
+
+        //Start Test
         int room[][];
 
-        room = boundaryWalk(time, appl);
+        room = boundaryWalk(time, appl, homePosR, homePosC);
 
-        int i =0, j=0;
+        for(int i =0;i<=lR/RobotLen+1+homePosR;i++){
+            for(int j=0; j<=lC/RobotLen+1+homePosC; j++){
+                System.out.print(room[i][j]);
+            }
+            System.out.print("\n");
+        }
+
+
+        int i=homePosR, j=homePosC, count=0;
         while (room[i][j] == 1){
             i++;
+            count++;
         }
         if(room[--i][++j]==0)
             fail();
-        while(room[i][j]==1)
-                    j++;
-        if(room[++i][--j]==0)
+        while(room[i][j]==1) {
+            j++;
+            count++;
+        }
+        if(room[--i][--j]==0)
             fail();
-        while (room[i][j] == 1 && i>=0)
+        while (room[i][j] == 1 && i>0) {
             i--;
-        if(room[++i][--j]==0)
-            fail();
-        while (room[i][j] == 1 && j>=0)
-            j--;
+            count++;
+        }
+        if (++i==homePosR) {
+            if (room[i][--j] == 0)
+                fail("Errore in angolo nord-est");
+        }else
+            fail("Robot oltre la riga di partenza!");
 
+        while (room[i][j] == 1 && j>0) {
+            j--;
+            count++;
+        }
+        if (++j!=homePosC)
+            fail("Errore Robot non tornato nel punto di partenza: Room["+i+","+j+"]");
+        else
+            count--;
+
+        System.out.print("D: "+ count*RobotLen +"m\tL: "+ perimetro+"m\n");
+        if((count*RobotLen) != perimetro)
+            fail("d!=p -> La distanza percorsa dal robot non corrisponde alla lunghezza perimetro!");
     }
 
     private static final int SUD = 0;
@@ -56,12 +87,14 @@ public class TestPlan3BoundaryWalk {
     private static final int NORD = 2;
     private static final int OVEST = 3;
 
-    public int[][] boundaryWalk(int time, MoveVirtualRobot robot){
+    public int[][] boundaryWalk(int time, MoveVirtualRobot robot, int homeR, int homeC){
         int room[][] = new int[200][200];
 
+        room[homeR][homeC] = 2;
         int direction = 0;
-        int r=0,c=0;
+        int r=homeR,c=homeC;
         boolean moveFailed;
+
         for(int i=0; i<4; i++) {
             do {
                 moveFailed = robot.moveForward(time);
@@ -87,12 +120,6 @@ public class TestPlan3BoundaryWalk {
                 direction++;
             }else
                 assertTrue(!moveFailed);
-        }
-        for(int i =0;i<10;i++){
-            for(int j=0; j<10; j++){
-                System.out.print(room[i][j]);
-            }
-            System.out.print("\n");
         }
 
         return room;
